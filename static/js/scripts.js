48e904e6b7327b071d4128f3a4e84dcf298da238
Ryan Rampersad <ryan.rampersad@gmail.com>
2017-04-27 22:00:05 -0500

    Ribbon

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -33,20 +33,15 @@ function debounce(func, wait, immediate) {
   document.addEventListener('touchmove', function (e) {
       // e.preventDefault();
   });
-  var c = document.getElementsByTagName('canvas');
-  if (c.length == 0) {
-    console.log(c);
-    // if the canvas is not here, leave
-    // // this will need adjusting later
+  var target = document.getElementById('ribbon');
+  if (!target) {
     return;
   }
-  c = c[0];
-  var x = c.getContext('2d'),
+  var x = target.getContext('2d'),
       pr = window.devicePixelRatio || 1,
       w = window.innerWidth,
       h = window.innerHeight,
       f = 90,
-      q,
       m = Math,
       r = 0,
       u = m.PI*2,
@@ -56,33 +51,7 @@ function debounce(func, wait, immediate) {
   c.height = h*pr;
   x.scale(pr, pr);
   x.globalAlpha = 0.4;
-  function draw(){
-      x.clearRect(0,0,w,h);
-      q=[{x:0,y:h*.7+f},{x:0,y:h*.7-f}];
-      while(q[1].x<w+f) {
-        draw_piece(q[0], q[1]);
-      }
-  }
-  function draw_piece(i,j){
-      x.beginPath();
-      x.moveTo(i.x, i.y); // q0-top
-      x.lineTo(j.x, j.y); // q1-bottom
-      var k = j.x + (z()*2-0.35)*f,
-          n = y(j.y);
-      x.lineTo(k, n);
-      x.closePath();
-      r-=u/-50;
 
-      // I realize the numbers here
-      // do not always make colorsense
-      // but that's apparently okay...
-      var color = '#'+(v(r)*127+128<<16).toString(16);
-
-      x.fillStyle = color;
-      x.fill();
-      q[0] = q[1];
-      q[1] = {x:k,y:n};
-  }
   function y(p){
       var t = p + ( z() * 2 - 1.2) * f;
       return (t > h || t < 0) ? y(p) : t;
@@ -105,38 +74,23 @@ function debounce(func, wait, immediate) {
             bottom: {x:k, y:n}
         });
       }
-    //   console.log("initial segments", segments);
       return segments;
   }
-
-    function get_new_destination(segments) {
-        var next = segments.map(function(segment, index) {
-            var _nnn = Object.assign({}, segment);
-            var _bbb = Object.assign({}, _nnn.bottom);
-            if (index == 0 || index == segments.length-1) {
-                return _nnn;
-            }
-
-            _bbb.x += (Math.random() - .5) * (Math.random() * 60);
-            _bbb.y += (Math.random() - .5) * (Math.random() * 60);
-            
-            _nnn.bottom = _bbb;
-            return _nnn;
-        });
-        return next;
-    }
 
   function get_colors(number) {
       var _r = 0;
       var _u = m.PI*2;
-      var _v = Math.tan;
+      var _v = Math.sin;
       var colors = [];
       for (var k = 0; k < number; k++) {
         _r -= _u / -50;
             // I realize the numbers here
             // do not always make colorsense
             // but that's apparently okay...
-        var color = '#'+(_v(_r)*127+128<<16).toString(16);
+        // var color = '#'+(_v(_r)*127+128<<16).toString(16);
+        var red = Math.round(90*Math.sin(_r)+120);
+        var color = 'rgb('+red+',0,0)';
+        console.log(red);
         colors.push(color);
       }
       return colors;
@@ -153,7 +107,7 @@ function debounce(func, wait, immediate) {
         var diff = [];
 
         for (var k = 0; k < segments.length; k++) {
-            var offset_x2 = (1/25)*Math.cos(k + counter/100);
+            var offset_x2 = (1/10)*Math.cos(k + counter/100);
             var offset_y2 = (1/4)*Math.sin(k + counter/100);
             var hh = segments[k];
             if (k !== 0 && k !== segments.length - 1) {
