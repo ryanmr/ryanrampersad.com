f448824653e8fb9fb99ff387b4f8e031fc744955
Ryan Rampersad <ryan.rampersad@daugherty.com>
2018-01-01 21:45:06 -0600

    Adding lang to custom html

--- src/html.js
+++ src/html.js
@@ -37,7 +37,7 @@ module.exports = class HTML extends React.Component {
       css = <style id="gatsby-inlined-css" dangerouslySetInnerHTML={{ __html: stylesStr }} />
     }
     return (
-      <html {...this.props.htmlAttributes}>
+      <html {...this.props.htmlAttributes} lang="en">
         <head>
           <meta charSet="utf-8" />
           <meta httpEquiv="x-ua-compatible" content="ie=edge" />
