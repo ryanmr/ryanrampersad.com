284805f34c51a7f1a85441765a0ddaf5b5f90408
Ryan Rampersad <ryan.rampersad@daugherty.com>
2018-01-01 23:03:47 -0600

    Adding GA

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,18 @@
 module.exports = {
   siteMetadata: {
     title: `Ryan Rampersad`,
-    description: 'Ryan Rampersad is a software engineer, web developer, consultant, podcaster and technology enthusiast.',
-    keywords: 'ryan rampersad, software engineer, web developer, consultant, java, php, javascript, rust, go, compilers, enthusiast, podcast'
+    description:
+      'Ryan Rampersad is a software engineer, web developer, consultant, podcaster and technology enthusiast.',
+    keywords:
+      'ryan rampersad, software engineer, web developer, consultant, java, php, javascript, rust, go, compilers, enthusiast, podcast'
   },
   plugins: [
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: 'UA-18192523-1'
+      }
+    },
     `gatsby-transformer-yaml`,
     {
       resolve: `gatsby-source-filesystem`,
