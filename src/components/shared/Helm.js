92fa64bccfe73b720179489a5a6ccffa503cf889
Ryan Rampersad <ryan.rampersad@gmail.com>
2021-05-23 21:54:21 -0500

    Tweak og/twitter meta

--- src/components/shared/Helm.js
+++ src/components/shared/Helm.js
@@ -6,16 +6,32 @@ export default function Helm({ children, description, title }) {
     <Helmet
       defaultTitle={`Ryan Rampersad`}
       titleTemplate={`%s › Ryan Rampersad`}>
-      {title && (
+      {title ? (
         <>
           <meta property="og:title" content={title} />
           <meta name="twitter:title" content={title} />
         </>
+      ) : (
+        <>
+          <meta property="og:title" content={`Ryan Rampersad`} />
+          <meta name="twitter:title" content={`Ryan Rampersad`} />
+        </>
       )}
-      {description && (
+      {description ? (
         <>
           <meta name="twitter:description" content={description} />
           <meta property="og:description" content={description} />
+        </>
+      ) : (
+        <>
+          <meta
+            name="twitter:description"
+            content={"Visit ryanrampersad.com for more"}
+          />
+          <meta
+            property="og:description"
+            content={"Visit ryanrampersad.com for more"}
+          />
         </>
       )}
       {children}
--- src/components/shared/SiteMeta.js
+++ src/components/shared/SiteMeta.js
@@ -2,7 +2,7 @@ import React from "react";
 import { graphql, useStaticQuery } from "gatsby";
 import { Helmet } from "react-helmet";
 import { useLocation } from "@reach/router";
-import ogImage from "../../assets/photos/og-image-1.png";
+import ogImage from "../../assets/photos/og-image-2.png";
 
 const query = graphql`
   query SiteMetaQuery {
@@ -40,6 +40,44 @@ export function SiteMeta() {
 
       <meta property="og:image" content={ogImage} />
       <meta property="twitter:image" content={ogImage} />
+      <meta
+        property="og:image:alt"
+        content="ryan rampersad over the red ribbon"
+      />
+      <meta property="og:image:width" content="1200" />
+      <meta property="og:image:height" content="600" />
+
+      {/* <meta property="fb:app_id" content="1401488693436528" />
+      <meta name="apple-itunes-app" content="app-id=1477376905" />
+      <meta
+        name="twitter:image:src"
+        content="https://opengraph.githubassets.com/15c4475b41e197f7c1d9d6bb7812f260d677bcc41bede57eeb041868faee6fef/ryanmr/ddns-linode"
+      />
+      <meta name="twitter:site" content="@github" />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:title" content="ryanmr/ddns-linode" />
+      <meta
+        name="twitter:description"
+        content="dynamically set a linode&#39;s A-record to a particular IP - ryanmr/ddns-linode"
+      />
+      <meta
+        property="og:image"
+        content="https://opengraph.githubassets.com/15c4475b41e197f7c1d9d6bb7812f260d677bcc41bede57eeb041868faee6fef/ryanmr/ddns-linode"
+      />
+      <meta
+        property="og:image:alt"
+        content="dynamically set a linode&#39;s A-record to a particular IP - ryanmr/ddns-linode"
+      />
+      <meta property="og:image:width" content="1200" />
+      <meta property="og:image:height" content="600" />
+      <meta property="og:site_name" content="GitHub" />
+      <meta property="og:type" content="object" />
+      <meta property="og:title" content="ryanmr/ddns-linode" />
+      <meta property="og:url" content="https://github.com/ryanmr/ddns-linode" />
+      <meta
+        property="og:description"
+        content="dynamically set a linode&#39;s A-record to a particular IP - ryanmr/ddns-linode"
+      /> */}
     </Helmet>
   );
 }
