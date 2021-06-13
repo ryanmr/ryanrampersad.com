3adabcf8c10444ec670cac2a50f4fb663205dbf2
Ryan Rampersad <ryan.rampersad@gmail.com>
2021-06-13 09:27:22 -0500

    Updates SiteMeta

--- src/components/shared/SiteMeta.js
+++ src/components/shared/SiteMeta.js
@@ -35,11 +35,11 @@ export function SiteMeta() {
         },
         { name: "keywords", content: site.siteMetadata.keywords },
       ]}>
+      <meta property="og:site_name" content={site.siteMetadata.title} />
       <meta property="og:url" content={url} />
-      <meta name="twitter:creator" content={`ryanmr`} />
 
+      <meta property="og:type" content="object" />
       <meta property="og:image" content={ogImage} />
-      <meta property="twitter:image" content={ogImage} />
       <meta
         property="og:image:alt"
         content="ryan rampersad over the red ribbon"
@@ -47,37 +47,16 @@ export function SiteMeta() {
       <meta property="og:image:width" content="1200" />
       <meta property="og:image:height" content="600" />
 
-      {/* <meta property="fb:app_id" content="1401488693436528" />
-      <meta name="apple-itunes-app" content="app-id=1477376905" />
-      <meta
-        name="twitter:image:src"
-        content="https://opengraph.githubassets.com/15c4475b41e197f7c1d9d6bb7812f260d677bcc41bede57eeb041868faee6fef/ryanmr/ddns-linode"
-      />
-      <meta name="twitter:site" content="@github" />
+      <meta property="twitter:image" content={ogImage} />
+
+      <meta name="twitter:creator" content={`ryanmr`} />
+      <meta name="twitter:site" content="@ryanmr" />
       <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:title" content="ryanmr/ddns-linode" />
+      <meta name="twitter:title" content="ryanrampersad.com" />
       <meta
         name="twitter:description"
-        content="dynamically set a linode&#39;s A-record to a particular IP - ryanmr/ddns-linode"
+        content={site.siteMetadata.description}
       />
-      <meta
-        property="og:image"
-        content="https://opengraph.githubassets.com/15c4475b41e197f7c1d9d6bb7812f260d677bcc41bede57eeb041868faee6fef/ryanmr/ddns-linode"
-      />
-      <meta
-        property="og:image:alt"
-        content="dynamically set a linode&#39;s A-record to a particular IP - ryanmr/ddns-linode"
-      />
-      <meta property="og:image:width" content="1200" />
-      <meta property="og:image:height" content="600" />
-      <meta property="og:site_name" content="GitHub" />
-      <meta property="og:type" content="object" />
-      <meta property="og:title" content="ryanmr/ddns-linode" />
-      <meta property="og:url" content="https://github.com/ryanmr/ddns-linode" />
-      <meta
-        property="og:description"
-        content="dynamically set a linode&#39;s A-record to a particular IP - ryanmr/ddns-linode"
-      /> */}
     </Helmet>
   );
 }
