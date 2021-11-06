517baf0391e90f767092668c5129bcd471250359
Ryan Rampersad <ryan.rampersad@gmail.com>
2021-11-05 19:48:29 -0500

    Fixes fragment rendering bug

--- src/components/shared/Helm.js
+++ src/components/shared/Helm.js
@@ -1,29 +1,43 @@
 import React from "react";
 import { Helmet } from "react-helmet";
 
-export default function Helm({ children, description, title }) {
+// this gets around a documented issue
+// of having fragments inside of a react-helmet enclosure
+// though prior to this commit, the fragment approach worked
+// https://github.com/nfl/react-helmet/issues/342
+function LocalHelmet({ children, ...props }) {
   return (
     <Helmet
+      {...props}
       defaultTitle={`Ryan Rampersad`}
       titleTemplate={`%s › Ryan Rampersad`}>
+      {children}
+    </Helmet>
+  );
+}
+
+export default function Helm({ children, description, title }) {
+  return (
+    <>
       {title ? (
-        <>
+        <LocalHelmet>
           <meta property="og:title" content={title} />
           <meta name="twitter:title" content={title} />
-        </>
+        </LocalHelmet>
       ) : (
-        <>
-          <meta property="og:title" content={`Ryan Rampersad`} />
-          <meta name="twitter:title" content={`Ryan Rampersad`} />
-        </>
+        <LocalHelmet>
+          <meta property="og:title" content={title} />
+          <meta name="twitter:title" content={title} />
+        </LocalHelmet>
       )}
+
       {description ? (
-        <>
+        <LocalHelmet>
           <meta name="twitter:description" content={description} />
           <meta property="og:description" content={description} />
-        </>
+        </LocalHelmet>
       ) : (
-        <>
+        <LocalHelmet>
           <meta
             name="twitter:description"
             content={"Visit ryanrampersad.com for more"}
@@ -32,9 +46,9 @@ export default function Helm({ children, description, title }) {
             property="og:description"
             content={"Visit ryanrampersad.com for more"}
           />
-        </>
+        </LocalHelmet>
       )}
-      {children}
-    </Helmet>
+      <LocalHelmet>{children}</LocalHelmet>
+    </>
   );
 }
