30eec363898a92c50fc907458b8aa99e1873872d
Ryan Rampersad <ryan.rampersad@gmail.com>
2023-01-22 20:49:37 -0600

    Adds About

---
+++ src/data/home.tsx
@@ -0,0 +1,12 @@
+export const subject: string = `I am a senior software engineer and architect, web developer, principal consultant, podcaster, and technology enthusiast.`;
+
+export const bio: string = `
+For [over a decade](https://github.com/ryanmr?bio), I have followed
+emerging technologies and trends and written code for fun and profit. I
+graduated from the University of Minnesota &mdash; Twin Cities with a
+Bachelors of Computer Science, with a focus on software engineering,
+systems and compilers. While at the _U of M_, I founded my [podcasting
+network](http://thenexus.tv?bio), and since then we have recorded more
+than 1300 episodes. I [love my work](/history?bio) as a senior software engineer and architect and principal and
+consultant.
+`.trim();
