841fab94b2f01e4b8fadf0c2030815c505754166
Ryan Rampersad <ryan.rampersad@gmail.com>
2023-01-29 00:13:47 -0600

    Adds history helper

---
+++ src/components/history/history.tsx
@@ -0,0 +1,40 @@
+import { parseISO } from "date-fns";
+
+export interface Entry {
+  description: string;
+  workTags: string;
+  topicTags: string;
+}
+
+export interface Section {
+  when: number;
+  entries: Entry[];
+}
+
+export type Sections = Section[];
+
+export function getSections(workLogData: any): Sections {
+  const source = workLogData;
+  const sections = source.map((node: any) => {
+    const when = node.when;
+
+    return {
+      ...node,
+      entries: node.entries.map((entry: any) => {
+        const description = entry.description;
+        const workTags = entry.work_tags;
+        const topicTags = entry.topic_tags;
+
+        return { when, description, workTags, topicTags };
+      }),
+      time: parseISO(node.when).getTime(),
+    };
+  });
+
+  // dangerously asset the type
+  return sections as Sections;
+}
+
+function optimize() {
+  return;
+}
