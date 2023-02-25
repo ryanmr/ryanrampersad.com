a6a7198455f037eaf4aba6666c161ac55a991dd7
Ryan Rampersad <ryan.rampersad@gmail.com>
2023-02-25 00:30:37 -0600

    Tweaks bio

--- src/data/home.tsx
+++ src/data/home.tsx
@@ -7,6 +7,5 @@ graduated from the University of Minnesota &mdash; Twin Cities with a
 Bachelors of Computer Science, with a focus on software engineering,
 systems and compilers. While at the _U of M_, I founded my [podcasting
 network](https://thenexus.tv?bio), and since then we have recorded more
-than 1300 episodes. I [love my work](/history?bio) as a senior software engineer and architect and principal and
-consultant.
+than 1300 episodes. I [love my work](/history?bio) as a senior software engineer and architect and principal consultant.
 `.trim();
