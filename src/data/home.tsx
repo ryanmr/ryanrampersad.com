063da21bcb22b772ec9b40bc33dcb3db22e2d09f
Ryan Rampersad <ryan.rampersad@gmail.com>
2023-02-24 20:53:14 -0600

    Fixes nexus link

--- src/data/home.tsx
+++ src/data/home.tsx
@@ -6,7 +6,7 @@ emerging technologies and trends and written code for fun and profit. I
 graduated from the University of Minnesota &mdash; Twin Cities with a
 Bachelors of Computer Science, with a focus on software engineering,
 systems and compilers. While at the _U of M_, I founded my [podcasting
-network](http://thenexus.tv?bio), and since then we have recorded more
+network](https://thenexus.tv?bio), and since then we have recorded more
 than 1300 episodes. I [love my work](/history?bio) as a senior software engineer and architect and principal and
 consultant.
 `.trim();
