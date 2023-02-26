07c4810daf9b64223c14590cd527f719f11a9409
Ryan Rampersad <ryan.rampersad@gmail.com>
2023-02-26 02:14:12 -0600

    Refactoring

--- src/components/decorations/fence-script.ts
+++ src/components/decorations/fence-script.ts
@@ -17,16 +17,6 @@ function setupFence(element: HTMLElement) {
     "(prefers-reduced-motion: reduce)"
   ).matches;
 
-  // const settingDuration = element.dataset.spiralBgTiming
-  //   ? safeParseInt(element.dataset.spiralBgTiming, 30)
-  //   : 30;
-  // let duration = settingDuration * 1000;
-  // const durationDecay = duration / (paths?.length ?? 20); // relative to the number of paths
-
-  // if (prefersReducedMotion.matches === true) {
-  //   duration = duration * 3; // should run 3x slower when prefers reduced motion is active
-  // }
-
   lines?.forEach((line) => {
     const r = Math.random();
     const shouldAnimate = r < 0.5;
